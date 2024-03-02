d4ad8e89ba09a7d0e46a15291130bc321fcd1652
Eulier <eulier@toolboxos.com>
2024-03-02 14:16:01 -0300

    handle with no "filters" query params

--- src/handlers/middleware/handleInputError.ts
+++ src/handlers/middleware/handleInputError.ts
@@ -2,17 +2,23 @@ import { FilterClauseType } from "../../types/filter";
 
 
 export const handleInputErrors = (req, res, next) => {
-    // Caveat, potential bottleneck, for very larger filters object, handling very large n° requests
-    const filters : FilterClauseType[]  = JSON.parse(req.query.filters)
-    const conditions = ['equals', 'does_not_equal','greater_than','less_than']
-
-    const isValid = filters.every( (filter) => conditions.includes( filter.condition ) )
     
-    if(!isValid){
-        res.status(400)
-        res.json({msg: "Invalid condition. Allow conditions : 'equals' | 'does_not_equal' | 'greater_than' | 'less_than'"})
+    if (req.query.filters) {
+        // Caveat, potential bottleneck, for very larger filters object, handling very large n° requests
+        const filters : FilterClauseType[]  = JSON.parse(req.query.filters)
+        const conditions = ['equals', 'does_not_equal','greater_than','less_than']
+
+        const isValid = filters.every( (filter) => conditions.includes( filter.condition ) )
+        
+        if(!isValid){
+            res.status(400)
+            res.json({msg: "Invalid condition. Allow conditions : 'equals' | 'does_not_equal' | 'greater_than' | 'less_than'"})
+        } else {
+            req.query.filters = filters
+            next()
+        }
     } else {
-        req.query.filters = filters
+        req.query.filters = []
         next()
     }
 
